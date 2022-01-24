307a45a4f43520ce1fc4d03f563b9e54bb552854
ShardaJuhi21 <91040711+ShardaJuhi21@users.noreply.github.com>
2022-01-24 21:15:25 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -8,6 +8,9 @@ int networkAlertStub(float celcius) {
     // Return 200 for ok
     // Return 500 for not-ok
     // stub always succeeds and returns 200
+    if(celcius < 200 || celcius > 200){
+        return celcius;
+    }
     return 200;
 }
 
