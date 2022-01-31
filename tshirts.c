3394665e44b3654d69ab6b3a1de14c7a994c4135
ShardaJuhi21 <91040711+ShardaJuhi21@users.noreply.github.com>
2022-01-31 21:53:28 +0530

    Update tshirts.c

--- tshirts.c
+++ tshirts.c
@@ -1,26 +1,47 @@
 #include <stdio.h>
 #include <assert.h>
 
-char size(int cms) {
+
+char size(int cms, void (*fpPrintToConsole)()) {
     char sizeName = '\0';
     if(cms < 38) {
         sizeName = 'S';
+        fpPrintToConsole();
     } else if(cms > 38 && cms < 42) {
         sizeName = 'M';
-    } else if(cms > 42) {
+        fpPrintToConsole();
+    } else if(cms > 42 && cms < 47) {
         sizeName = 'L';
+       fpPrintToConsole();
+    }else{
+        sizeName = "Z"; //size not available
+        fpPrintToConsole();
     }
     return sizeName;
 }
 
+void printToConsoleAvailableSize(){
+ printf(" Tshirt size available");
+}
+void printToConsoleUnavailableSize(){
+ printf(" Tshirt size not available");
+}
 
+void testAvailableTshirtSize( int tshirtSize){
+   size(tshirtSize, printToConsoleAvailableSize );
+}
+
+
+void testAvailableTshirtSize( int tshirtSize){
+   size(tshirtSize, printToConsoleUnavailableSize );
+}
 
 int main() {
     assert(size(37) == 'S');
     assert(size(40) == 'M');
     assert(size(43) == 'L');
-    //extra large
-    assert(size(47) == 'Z');
-    printf("All is well (maybe!)\n");
+    assert(size(48) == 'Z');
+    testAvailableTshirtSize( 38);
+    testAvailableTshirtSize( 48);
     return 0;
 }
