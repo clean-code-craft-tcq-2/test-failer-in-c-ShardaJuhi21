ecbdc463c7d89c696f7223fc0ec1b3a5af7d89bc
ShardaJuhi21 <91040711+ShardaJuhi21@users.noreply.github.com>
2022-02-01 11:21:15 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -22,7 +22,7 @@ void stubprintToConsole(){
  
 void alertCheck( float (*fpalertInCelcius)(float), float farenheit,void (*fpPrintToConsole)(),int (*fpnetworkAlertStub)(float)){
     float localfarenheit =farenheit;
-    float localCelsius = fpPrintalertInCelcius(localfarenheit);
+    float localCelsius = fpalertInCelcius(localfarenheit);
     int returnCode = fpnetworkAlertStub(localCelsius);
     if (returnCode != 200) {
         // non-ok response is not an error! Issues happen in life!
