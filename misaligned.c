78f8f4117580c163250cb355f9edcd568b35b6c0
ShardaJuhi21 <91040711+ShardaJuhi21@users.noreply.github.com>
2022-02-01 08:07:00 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -4,39 +4,47 @@
 const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
 const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 
-int printColorMap(void (*fpPrintToConsole)(int majorColorIndex, int minorColorIndex)) {
+int printColorMap(void (*fpPrintToConsole)(const ColorPair* colorPair)) {
     int i = 0, j = 0;
-    for(i = 0; i < majorColorIndex; i++) {
-        for(j = 0; j < minorColorIndex; j++) {
-           fpPrintToConsole(i,j);
+    for(i = 0; i < colorPair.numberOfMajorColors; i++) {
+        for(j = 0; j < colorPair.numberOfMinorColors; j++) {
+           fpPrintToConsole(colorPair);
         }
     }
     return i * j;
 }
 
-void printToConsole(int majorColorIndex , int minorColorIndex ){
+void printToConsole(const ColorPair* colorPair ){
     
-     printf("%d | %s | %s\n", majorColorIndex * 5 + minorColorIndex, majorColor[majorColorIndex], minorColor[minorColorIndex]);
+     printf("%d | %s | %s\n", colorPair.numberOfMajorColors * 5 + colorPair.numberOfMinorColors, majorColor[colorPair.numberOfMajorColors], minorColor[colorPair.numberOfMinorColors]);
     
 }
 
 // stub
-
+int numberOfMajorColors =
+    sizeof(majorColor) / sizeof(majorColor[0]);
+int numberOfMinorColors =
+    sizeof(minorColor) / sizeof(minorColor[0]);
 typedef struct {
     int PairNumber = 0 
-    char majorColor[];
-    char minorColor[];
+    char MajorColorNames[15];
+    char MinorColorNames[15];
+    numberOfMajorColors;
+    numberOfMinorColors;
 } ColorPair;
 
 
-void stubPrintToConsole(int numberOfMajorColors, int numberOfMinorColors ){
+void stubPrintToConsole(const ColorPair* colorPair ){
  ColorPair.PairNumber++;
+ MajorColorNames[colorPair->majorColor],
+ MinorColorNames[colorPair->minorColor];
+    
 }
 
 
 
 int main() {
-    int result = printColorMap(stubPrintToConsole(5, 5));
+    int result = printColorMap(stubPrintToConsole(const ColorPair* colorPair));
     assert(ColorPair.PairName == result);
     printf("All is well (maybe!)\n");
     return 0;
