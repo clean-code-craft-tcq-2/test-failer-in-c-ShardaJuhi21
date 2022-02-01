7223b1c1c428e64b229090c133bd150ac8dc2b1c
ShardaJuhi21 <91040711+ShardaJuhi21@users.noreply.github.com>
2022-02-01 07:52:59 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,21 +1,43 @@
 #include <stdio.h>
 #include <assert.h>
 
-int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
+int printColorMap(void (*fpPrintToConsole)(int majorColorIndex, int minorColorIndex)) {
     int i = 0, j = 0;
-    for(i = 0; i < 5; i++) {
-        for(j = 0; j < 5; j++) {
-            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+    for(i = 0; i < majorColorIndex; i++) {
+        for(j = 0; j < minorColorIndex; j++) {
+           fpPrintToConsole(i,j);
         }
     }
     return i * j;
 }
 
+void printToConsole(int majorColorIndex , int minorColorIndex ){
+    
+     printf("%d | %s | %s\n", majorColorIndex * 5 + minorColorIndex, majorColor[majorColorIndex], minorColor[minorColorIndex]);
+    
+}
+
+// stub
+
+typedef struct {
+    int PairNumber = 0 
+    char majorColor[];
+    char minorColor[];
+} ColorPair;
+
+
+void stubPrintToConsole(int numberOfMajorColors, int numberOfMinorColors ){
+ ColorPair.PairNumber++;
+}
+
+
+
 int main() {
-    int result = printColorMap();
-    assert(result == 25);
+    int result = printColorMap(stubPrintToConsole(5, 5));
+    assert(ColorPair.PairName == result);
     printf("All is well (maybe!)\n");
     return 0;
 }
