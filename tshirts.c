42b64bc2a2f5a88f06aff1dc423f233c667f5287
ShardaJuhi21 <91040711+ShardaJuhi21@users.noreply.github.com>
2022-02-03 19:08:06 +0530

    Update tshirts.c

--- tshirts.c
+++ tshirts.c
@@ -47,7 +47,7 @@ void stubprintToConsoleForUnvailableSize(char sizeName){
 
 int main() {
     size(39, printToConsoleAvailableSize);
-    size(48, printToConsoleAvailableSize);
+    //size(48, printToConsoleAvailableSize);
     size(39, stubprintToConsoleForAvailableSize);
     assert(Count.availableSizeCount ==1);
     assert(SizeName =='M');
